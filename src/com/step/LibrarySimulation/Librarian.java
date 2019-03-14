039969be664c85bcf665965e570a94a99f3b186e
Rishabgh <rishabgh@in-Rishabgh.local>
2019-03-14 13:03:31 +0530

    LibrarySimulation - extracted librarian methods to librarian

--- src/com/step/LibrarySimulation/Librarian.java
+++ src/com/step/LibrarySimulation/Librarian.java
@@ -1,4 +1,61 @@
 package com.step.LibrarySimulation;
 
+import java.util.List;
+
 public class Librarian {
+    private final Library library;
+
+    public Librarian(Library library) {
+        this.library = library;
+    }
+
+    private boolean isReaderValid(BookReader candidate) {
+        return library.getBookReaders().contains(candidate);
+    }
+
+
+    public void giveBookToReader(BookReader reader, Book book) {
+        if (isReaderValid(reader) && library.hasBook(book)) {
+            library.removeBook(book);
+            reader.addNewBook(book);
+            return;
+        }
+        System.out.println("Invalid Reader or book name");
+    }
+
+    public void returnBookFromReader(BookReader reader, Book book){
+        if(isReaderValid(reader) && reader.hasBook(book)){
+            System.out.println(book);
+            reader.removeBook(book);
+            library.addNewBook(book);
+            return;
+        }
+        System.out.println("Invalid Reader or reader don't have this book");
+    }
+
+    public BookReader currentReaderOfBook(Book book){
+        for (BookReader reader:library.getBookReaders()) {
+            if(reader.hasBook(book)){
+                return reader;
+            }
+        }
+        return null;
+    }
+
+    public List<Book> checkAllBooksOfReader(BookReader reader){
+        return reader.getBooks();
+    }
+
+    public boolean isBookRemoved(Book book){
+        return library.getRemovedBooks().contains(book);
+    }
+
+    public void bringBack(Book book){
+        if(library.getRemovedBooks().contains(book)){
+            library.removeBook(book);
+            library.addNewBook(book);
+            return;
+        }
+        System.out.println("Book does not exist in removed list");
+    }
 }
--- src/com/step/LibrarySimulation/Library.java
+++ src/com/step/LibrarySimulation/Library.java
@@ -9,66 +9,21 @@ public class Library extends Common {
 
     public Library() {}
 
-    private boolean isReaderValid(BookReader candidate) {
-        return this.bookReaders.contains(candidate);
-    }
-
-    public void addReader(BookReader reader) {
-        this.bookReaders.add(reader);
-    }
-
-    public void giveBookToReader(BookReader reader, Book book) {
-        if (isReaderValid(reader) && hasBook(book)) {
-            super.removeBook(book);
-            reader.addNewBook(book);
-            return;
-        }
-        System.out.println("Invalid Reader or book name");
-    }
-
-    public void returnBookFromReader(BookReader reader, Book book){
-        if(isReaderValid(reader) && reader.hasBook(book)){
-            System.out.println(book);
-            reader.removeBook(book);
-            super.addNewBook(book);
-            return;
-        }
-        System.out.println("Invalid Reader or reader don't have this book");
-    }
-
-    public BookReader currentReaderOfBook(Book book){
-        for (BookReader reader:this.bookReaders) {
-            if(reader.hasBook(book)){
-                return reader;
-            }
-        }
-        return null;
-    }
-
-    public List<Book> checkAllBooksOfReader(BookReader reader){
-        return reader.getBooks();
-    }
-
     @Override
     public void removeBook(Book book) {
         super.removeBook(book);
         this.removedBooks.add(book);
     }
 
-    public boolean isRemoved(Book book){
-        return removedBooks.contains(book);
+    public void addReader(BookReader reader) {
+        this.bookReaders.add(reader);
+    }
+
+    public List<BookReader> getBookReaders() {
+        return this.bookReaders;
     }
 
     public List<Book> getRemovedBooks() {
         return this.removedBooks;
-    }
-
-    public void bringBack(Book book){
-        if(this.removedBooks.contains(book)){
-            this.removedBooks.remove(book);
-            super.addNewBook(book);
-            return;
-        }
-        System.out.println("Book does not exist in removed list");
     }
 }
